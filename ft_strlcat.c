fb6f9662ce7c3a964154725244689e6f9336a99c
Raquel Larielis <rqlprodigy@gmail.com>
2024-09-19 17:32:16 +0200

    ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 16:05:28 by racamach          #+#    #+#             */
+/*   Updated: 2024/09/19 16:40:10 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	strlcat(char *dest, const char *src, size_t n)
+{
+	size_t	dsize;
+	size_t	len;
+	size_t	res;
+
+	dsize = ft_strlen(dest);
+	len = ft_strlen(src);
+	res = dsize + len;
+	if (dsize < n)
+	{
+		dest += dsize;
+		n -= dsize;
+		if (len >= n)
+			len = n - 1;
+		dest[len] = '\0';
+		ft_memcpy(dest, src, len);
+	}
+	return (res);
+}
