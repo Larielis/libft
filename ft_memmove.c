47daf45b00c7a0e4b3153aea92283107dd608125
Raquel Larielis <rqlprodigy@gmail.com>
2024-09-19 15:47:08 +0200

    ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 14:10:04 by racamach          #+#    #+#             */
+/*   Updated: 2024/09/19 15:46:58 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t count)
+{
+	const unsigned char	*s;
+	unsigned char		*tmp;
+
+	if (dest < src || src + count <= dest)
+		return (ft_memcpy(dest, src, count));
+	if (dest > src)
+	{
+		s = src + count;
+		tmp = dest + count;
+		while (count--)
+			*--tmp = *--s;
+	}
+	return (dest);
+}
