8890135456dbb22afd8a3f52cd33ac94b2275468
Raquel Larielis <rqlprodigy@gmail.com>
2024-09-19 17:52:43 +0200

    ft_strtchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,27 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 17:15:07 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/19 17:15:32 by racamach         ###   ########.fr       */
+/*   Updated: 2024/09/19 17:50:06 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
 char	*ft_strrchr(const char *s, int c)
 {
-	
+	const char	*last;
+
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+		{
+			last = s;
+		}
+		*s++;
+	}
+	if (!last)
+	{
+		return (NULL);
+	}
+	return ((char *)s);
 }
