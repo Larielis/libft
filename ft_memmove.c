cada48ddaad0a3587293d6e55e3a9e60a421a2a5
Larielis <racamach@student.42madrid.com>
2024-09-20 20:30:24 +0200

    ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:10:04 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/19 15:46:58 by racamach         ###   ########.fr       */
+/*   Updated: 2024/09/20 16:28:59 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t count)
 {
@@ -18,7 +19,9 @@ void	*ft_memmove(void *dest, const void *src, size_t count)
 	unsigned char		*tmp;
 
 	if (dest < src || src + count <= dest)
+	{
 		return (ft_memcpy(dest, src, count));
+	}
 	if (dest > src)
 	{
 		s = src + count;
