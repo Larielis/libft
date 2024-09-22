b749d8de7ad0547c87e58e72b84aaa6a8272821d
Larielis <racamach@student.42madrid.com>
2024-09-22 13:29:20 +0200

    memset

---
+++ ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 01:34:26 by racamach          #+#    #+#             */
+/*   Updated: 2024/09/22 11:59:43 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*tmp;
+
+	tmp = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		*tmp = (unsigned char)c;
+		i++;
+		tmp++;
+	}
+	return (s);
+}
