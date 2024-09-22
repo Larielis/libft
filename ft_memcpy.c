4ade58d5b98bcf08715369efdcddcb3509760f5d
Larielis <racamach@student.42madrid.com>
2024-09-22 13:29:12 +0200

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,22 +6,25 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:15:39 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/20 16:26:40 by racamach         ###   ########.fr       */
+/*   Updated: 2024/09/22 12:08:47 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	const unsigned char	*s;
 	unsigned char		*tmp;
 
-	s = src;
-	tmp = dest;
-	while (n--)
+	s = (const unsigned char *)src;
+	tmp = (unsigned char *)dest;
+	while (n)
 	{
-		*tmp++ = *s++;
+		*tmp = *s;
+		tmp++;
+		s++;
+		n--;
 	}
 	return (dest);
 }
