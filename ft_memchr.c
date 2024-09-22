770a2ffd1eae7bf96795d1bf8d1953530dd9789d
Larielis <racamach@student.42madrid.com>
2024-09-22 22:36:44 +0200

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 21:56:14 by racamach          #+#    #+#             */
+/*   Updated: 2024/09/22 22:17:13 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*tmp;
+	size_t				i;
+
+	i = 0;
+	tmp = (const unsigned char *)s;
+	while (i < n)
+	{
+		if (*tmp == (unsigned char)c)
+		{
+			return ((void *)tmp);
+		}
+		tmp++;
+		i++;
+	}
+	return (NULL);
+}
