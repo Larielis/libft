3473bacbbb56c0ab7d7427a2b9d1ff74aa2b6a38
Larielis <racamach@student.42madrid.com>
2024-09-25 00:24:12 +0200

    ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 16:05:28 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/22 20:51:45 by racamach         ###   ########.fr       */
+/*   Updated: 2024/09/25 00:22:41 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t n)
 {
 	size_t	dsize;
-	size_t	len;
+	size_t	ssize;
 	size_t	res;
 
 	dsize = ft_strlen(dest);
-	len = ft_strlen(src);
-	res = dsize + len;
-	if (dsize < n)
-	{
-		dest += dsize;
-		n -= dsize;
-		if (len >= n)
-			len = n - 1;
-		dest[len] = '\0';
-		ft_memcpy(dest, src, len);
-	}
+	ssize = ft_strlen(src);
+	res = dsize + ssize;
+	if (n == 0)
+		return (ssize);
+	if (dsize >= n)
+		return (ssize + n);
+	dest += dsize;
+	n -= dsize;
+	if (ssize >= n)
+		ssize = n - 1;
+	ft_memcpy(dest, src, ssize);
+	dest[ssize] = '\0';
 	return (res);
 }
