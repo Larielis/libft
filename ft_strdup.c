b38bc3c0cfd4ee9948dd79cd9c48e91b98df9cb7
Larielis <racamach@student.42madrid.com>
2024-09-29 22:51:55 +0200

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 13:12:09 by racamach          #+#    #+#             */
+/*   Updated: 2024/09/29 13:16:03 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*dup;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	len = ft_strlen(src);
+	dup = (char *)malloc((len + 1) * sizeof(char));
+	if (!dup)
+	{
+		return (NULL);
+	}
+	while (i < len)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
