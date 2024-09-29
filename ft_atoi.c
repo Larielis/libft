31f49d0a353e86b6f9232b4d036731fdea6b5ac3
Larielis <racamach@student.42madrid.com>
2024-09-29 22:53:15 +0200

    ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,19 +6,26 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:13:04 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/25 18:06:47 by racamach         ###   ########.fr       */
+/*   Updated: 2024/09/29 13:41:44 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Converts a string to an integer.
+ * 
+ * This function takes a string as input and converts it to an integer.
+ * It handles optional leading whitespace, an optional sign, and numeric digits.
+ * 
+ * @param nptr The string to be converted.
+ * @return The converted integer.
+ */
 int	ft_atoi(const char *nptr)
 {
 	int	i;
 	int	sign;
 	int	result;
-	int	minus_count;
 
 	i = 0;
-	minus_count = 0;
 	sign = 1;
 	result = 0;
 	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
