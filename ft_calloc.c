7484828d21ab7f86643d19d85f582ee14313d706
Larielis <racamach@student.42madrid.com>
2024-09-29 22:52:29 +0200

    ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,18 +6,22 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:24:34 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/25 18:46:04 by racamach         ###   ########.fr       */
+/*   Updated: 2024/09/29 13:24:59 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief 
+/// @param nmemb
+/// @param size
+/// @return
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*tmp;
 
-	if(nmemb == 0 || size  == 0)
-		return  (NULL);
+	if (nmemb == 0 || size == 0 || nmemb > SIZE_MAX / size)
+		return (NULL);
 	tmp = malloc(nmemb * size);
 	if (!tmp)
 		return (NULL);
