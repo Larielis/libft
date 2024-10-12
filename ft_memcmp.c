7002f18f68e1f0a1c774e6f848e9587cacf92e6c
Larielis <racamach@student.42madrid.com>
2024-10-12 12:09:54 +0200

    Refactor ft_memcmp function to improve readability and type casting

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:19:03 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/25 16:25:59 by racamach         ###   ########.fr       */
+/*   Updated: 2024/10/12 11:51:05 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	const unsigned char	*tmp1;
 	const unsigned char	*tmp2;
 
-	tmp1 = s1;
-	tmp2 = s2;
+	tmp1 = (const unsigned char *)s1;
+	tmp2 = (const unsigned char *)s2;
 	i = 0;
-	while (i < n && tmp1[i] != '\0' && tmp2[i] != '\0')
+	while (i < n)
 	{
 		if (tmp1[i] != tmp2[i])
 		{
 			return (tmp1[i] - tmp2[i]);
 		}
 		i++;
-	}
-	if (i < n)
-	{
-		if (tmp1[i] != tmp2[i])
-		{
-			return (tmp1[i] - tmp2[i]);
-		}
 	}
 	return (0);
 }
