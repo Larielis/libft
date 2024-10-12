8cf7d612e3c5de2c53ff48ae34868f64a7b9372a
Larielis <racamach@student.42madrid.com>
2024-10-12 12:13:17 +0200

    Refactor: ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 12:31:18 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/22 22:17:22 by racamach         ###   ########.fr       */
+/*   Updated: 2024/10/12 11:48:44 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && s1[i] != '\0' && s2[i] != '\0')
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
 		if (s1[i] != s2[i])
 		{
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		i++;
-	}
-	if (i < n)
-	{
-		if (s1[i] != s2[i])
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
 	}
 	return (0);
 }
