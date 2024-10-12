8f4742d47904d2138b4b0d2c7d030306c8a0385d
Larielis <racamach@student.42madrid.com>
2024-10-12 12:10:04 +0200

    Refactor ft_memmove function to improve readability and type casting

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,33 +6,32 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:10:04 by racamach          #+#    #+#             */
-/*   Updated: 2024/09/25 22:06:23 by racamach         ###   ########.fr       */
+/*   Updated: 2024/10/12 11:34:30 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t count)
 {
-	const unsigned char	*s;
-	unsigned char		*tmp;
+	const unsigned char	*src_ptr;
+	unsigned char		*dest_ptr;
 
 	if (!dest || !src)
 		return (NULL);
-	s = (const unsigned char *)src;
-	tmp = (unsigned char *)dest;
-	if (tmp < s || s + count <= tmp)
+	src_ptr = (const unsigned char *)src;
+	dest_ptr = (unsigned char *)dest;
+	if (dest_ptr < src_ptr || src_ptr + count <= dest_ptr)
 	{
 		return (ft_memcpy(dest, src, count));
 	}
-	if (dest > src)
+	if (dest_ptr > src_ptr)
 	{
-		s = src + count;
-		tmp = dest + count;
+		src_ptr += count;
+		dest_ptr += count;
 		while (count--)
 		{
-			*--tmp = *--s;
+			*--dest_ptr = *--src_ptr;
 		}
 	}
 	return (dest);
