e78c1debe44de4a3093314bfe026e016b4e243eb
Larielis <racamach@student.42madrid.com>
2024-10-12 12:13:31 +0200

    Added: ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 20:07:07 by racamach          #+#    #+#             */
+/*   Updated: 2024/10/11 20:07:51 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1)
+		s1++;
+	i = ft_strlen(s1);
+	while (i > 0 && ft_strchr(set, s1[i - 1]))
+		i--;
+	return (ft_substr(s1, 0, i));
+}
