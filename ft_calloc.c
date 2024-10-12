2707acef306728ef0baeb4511f690169a5908b2c
Larielis <racamach@student.42madrid.com>
2024-10-12 12:09:16 +0200

    Refactor ft_calloc function

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,31 +6,12 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:24:34 by racamach          #+#    #+#             */
-/*   Updated: 2024/10/04 22:04:01 by racamach         ###   ########.fr       */
+/*   Updated: 2024/10/12 11:53:56 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/**
- * @brief Allocates memory and initializes it to zero.
- *
- * The ft_calloc() function allocates memory for an array of `nmemb` elements 
- * of `size` bytes each and returns a pointer to the allocated memory. 
- * The memory is set to zero. 
- * If `nmemb` or `size` is 0, `ft_calloc` returns NULL.
- *
- * @param nmemb Number of elements to allocate.
- * @param size Size of each element.
- * @return A pointer to the allocated memory, which is suitably aligned for any 
- * kind of variable. On error, or if `nmemb` or `size` is 0, NULL is returned.
- *
- * @note This function checks for overflow when calculating the total size of 
- * the memory to allocate. It relies on `ft_bzero` to set the memory to zero.
- * @warning The behavior is undefined if the multiplication of `nmemb` and 
- * `size` would result in overflow.
- *
- */
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*tmp;
