53a48e98ab8e51c8f39456f342593c5b1e11bc7d
Larielis <racamach@student.42madrid.com>
2024-10-12 12:13:46 +0200

    Refactor: ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,12 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 19:09:52 by racamach          #+#    #+#             */
-/*   Updated: 2024/10/04 21:48:10 by racamach         ###   ########.fr       */
+/*   Updated: 2024/10/12 11:28:22 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/**
- *@brief Allocates (with `malloc`) and returns a substring from the string `s`.
- *@param s: The substring begins at index `start` and is of maximum size `len`.
- *@param start: The start index of the substring in the string `s`.
- *@param len: The maximum length of the substring.
- *@return The substring, or `NULL` if the allocation fails.
- */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
