84c5a081a60672ccb6dcecb73bcd7b23ae8910c3
Larielis <74860846+Larielis@users.noreply.github.com>
2024-10-14 08:06:10 +0000

    ft_lstdelone_bonus

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 06:52:18 by racamach          #+#    #+#             */
+/*   Updated: 2024/10/14 07:05:31 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst != NULL && del != NULL)
+	{
+		del(lst->content);
+		free(lst);
+	}
+}
