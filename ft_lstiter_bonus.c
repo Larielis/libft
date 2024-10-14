4ecf4918457bd960d02306088ca4432102f8b7f2
Larielis <74860846+Larielis@users.noreply.github.com>
2024-10-14 08:06:30 +0000

    ft_lstiter_bonus

---
+++ ft_lstiter_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 07:55:32 by racamach          #+#    #+#             */
+/*   Updated: 2024/10/14 07:56:05 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst != NULL && f != NULL)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
