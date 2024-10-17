562cca7512c37ecfc1a22c9b132c8882624d3518
Larielis <racamach@student.42madrid.com>
2024-10-17 21:24:36 +0200

    ft_lstadd_front

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 23:36:09 by racamach          #+#    #+#             */
-/*   Updated: 2024/10/12 23:37:17 by racamach         ###   ########.fr       */
+/*   Updated: 2024/10/13 09:13:49 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
+	new->next = *lst;
+	*lst = new;
 }
