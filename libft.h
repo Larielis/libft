c3dc38b831bec9f7e918b30ddcd3a0afc11cfb1e
Larielis <racamach@student.42madrid.com>
2024-10-17 21:25:28 +0200

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:12:56 by racamach          #+#    #+#             */
-/*   Updated: 2024/10/12 23:34:06 by racamach         ###   ########.fr       */
+/*   Updated: 2024/10/13 09:14:21 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,4 +61,6 @@ void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
 #endif
