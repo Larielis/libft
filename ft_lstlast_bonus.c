ddd8a34980f17667a121d7030c7cfa2af970c8ec
Larielis <racamach@student.42madrid.com>
2024-10-17 21:24:46 +0200

    ft_last_bonus

---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 09:14:48 by racamach          #+#    #+#             */
+/*   Updated: 2024/10/13 09:19:49 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*last_node;
+
+	while (lst)
+	{
+		last_node = lst;
+	}
+	return (lst);
+}
