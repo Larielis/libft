566af8fc76c2c8ba0be4e4b647603920dcb3d5e7
Raquel Larielis <rqlprodigy@gmail.com>
2024-10-21 20:05:21 +0200

    ft_isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,7 +12,104 @@
 
 #include "libft.h"
 
+/**
+ * @brief Checks if the given character is alphanumeric.
+ *
+ * This function checks if the character passed as an argument is either
+ * an alphabetic letter (a-z, A-Z) or a digit (0-9).
+ *
+ * @param c The character to be checked.
+ * @return Non-zero value if the character is alphanumeric, zero otherwise.
+ */
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
+}
+
+int main()
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		if(ft_isalnum(i))
+		{
+			printf("%c is alphanumeric \n", i);
+		}		
+		else
+		{
+			printf("%c is not alphanumeric \n", i);
+		}
+	}
+
+	for (char i = 'A'; i <= 'Z'; i++)
+	{
+		if(ft_isalnum(i))
+		{
+			printf("%c is alphanumeric \n", i);
+		}
+		else
+		{
+			printf("%c is not alphanumeric \n", i);
+		}
+	}
+
+	for (char i = '0'; i <= '9'; i++)
+	{
+		if(ft_isalnum(i))
+		{
+			printf("%c is alphanumeric \n", i);
+		}
+		else
+		{
+			printf("%c is not alphanumeric \n", i);
+		}
+	}
+
+	for (char i = 0; i <= 47; i++)
+	{
+		if(!ft_isalnum(i))
+		{
+			printf("%c is not alphanumeric \n", i);
+		}
+		else
+		{
+			printf("%c is alphanumeric \n", i);
+		}
+	}
+
+	for (char i = 58; i <= 64; i++)
+	{
+		if(!ft_isalnum(i))
+		{
+			printf("%c is not alphanumeric \n", i);
+		}
+		else
+		{
+			printf("%c is alphanumeric \n", i);
+		}
+	}
+
+	for (char i = 91; i <= 96; i++)
+	{
+		if(!ft_isalnum(i))
+		{
+			printf("%c is not alphanumeric \n", i);
+		}
+		else
+		{
+			printf("%c is alphanumeric \n", i);
+		}
+	}
+
+	for (int i = 123; i <= 127; i++)
+	{
+		if(!ft_isalnum(i))
+		{
+			printf("%c is not alphanumeric \n", i);
+		}
+		else
+		{
+			printf("%c is alphanumeric \n", i);
+		}
+	}
+	return 0;
 }
